3538c5a91ab30108b89b949bf7e557ec51178e42
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2021-04-11 15:19:06 +0300

    Update aim_trainer.py

--- aim_trainer.py
+++ aim_trainer.py
@@ -86,7 +86,7 @@ TARGET_RADIUS = 16
 MARKERS_RADIUS = 15
 
 #load images
-directory = os.path.dirname('enter path of images here')
+directory = os.path.dirname('Images//')
 target = pygame.image.load(os.path.join(directory, "target.jpg"))
 hitmarker = pygame.image.load(os.path.join(directory, "hitmarker.jpg"))
 missmarker = pygame.image.load(os.path.join(directory, "missmarker.jpg"))
